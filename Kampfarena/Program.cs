10d287928524cea041ff0925ab941c33c7ded793
AndreGSO <andree@gso.schule.koeln>
2020-09-23 16:23:08 +0200

    Welcome Methode eingebaut

--- Kampfarena/Program.cs
+++ Kampfarena/Program.cs
@@ -6,7 +6,18 @@ namespace Kampfarena
     {
         static void Main(string[] args)
         {
-            
+            welcome();
+        }
+
+        static void welcome()
+        {
+            Console.WriteLine("Willkommen in der brutalsten commandline Kampfarena.");
+            Console.WriteLine("Du hast die Wahl eine Kämpferklasse auszuwählen.");
+            Console.WriteLine("Jede Klasse hat dabei seine Vor- und Nachteile.");
+            Console.WriteLine("Bereit für die Auswahl? Drücke eine beliebige Taste!");
+            Console.WriteLine("-----------------------------------------------");
+            Console.ReadKey();
+            Console.WriteLine("Du kannst zwischen folgenden Klasse auswählen:");
         }
     }
 }
