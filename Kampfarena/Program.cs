8d8e74f8acfd397833e98c334c0d1f86dbe0cf06
FIA96ane <andree@schule.koeln>
2020-09-23 15:57:28 +0200

    Testnachrch

--- Kampfarena/Program.cs
+++ Kampfarena/Program.cs
@@ -7,6 +7,7 @@ namespace Kampfarena
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bitte Helden auswählen");
         }
     }
 }
